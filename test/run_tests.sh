d001f06792258e5d82a6b5a14177d1eb63fc0325
Alex Manko <oleksandr_manko@epam.com>
2019-08-22 06:16:35 +0300

    output is beautified;

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-echo -e "\n" current user and path info
-echo user ==> $(whoami); path ==> $(pwd)
+echo $'\n'current user and path info
+echo user ==> $(whoami)
+echo path ==> $(pwd)
 echo "[OK]"
 
-echo -e "\n" started scaning 10.1.0.1/24 network
+echo $'\n'started scaning 10.1.0.1/24 network
 nmap 10.1.0.1/24
 echo "[OK]"
 
-echo -e "\n" started scaning expected host 10.1.0.5
+echo $'\n'started scaning expected host 10.1.0.5
 nmap 10.1.0.5
 echo "[OK]"
 
-echo -e "\n" started ssh connection to the expected host via ssh
+echo $'\n'started ssh connection to the expected host via ssh
 USERNAME=$(sshpass -p EC2FakeUSer ssh -o StrictHostKeyChecking=no ec2-fake-user@10.1.0.5 whoami)
 
 if [ "$USERNAME" != "ec2-fake-user" ] 
 then
     exit 1
 else
-    echo "[OK]"
+    echo "[OK]"$'\n'
 fi
