d2075daea7ac8b7df8eeb6cb8c9df18ef82ca178
Alex Manko <oleksandr_manko@epam.com>
2019-08-22 05:40:00 +0300

    sshpass statement is chenged;

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -10,9 +10,6 @@ echo started scaning expected host 10.1.0.5
 nmap 10.1.0.5
 
 echo started ssh connection to the expected host via ssh 
-sshpass -p EC2FakeUSer ssh ec2-fake-user@10.1.0.5
-
-echo user ==> $(whoami)
-echo path ==> $(pwd)
+sshpass -p EC2FakeUSer ssh -o StrictHostKeyChecking=no ec2-fake-user@10.1.0.5 whoami
 
 exit
