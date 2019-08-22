e63d5f412d48ab10f25a61552e1e94a800b38063
Alex Manko <oleksandr_manko@epam.com>
2019-08-22 06:01:14 +0300

    output is beautified; assertion is added;

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-echo user ==> $(whoami)
-echo path ==> $(pwd)
+echo -e "\n" current user and path info
+echo user ==> $(whoami); path ==> $(pwd)
+echo "[OK]"
 
-echo started scaning 10.1.0.1/24 network
+echo -e "\n" started scaning 10.1.0.1/24 network
 nmap 10.1.0.1/24
+echo "[OK]"
 
-echo started scaning expected host 10.1.0.5
+echo -e "\n" started scaning expected host 10.1.0.5
 nmap 10.1.0.5
+echo "[OK]"
 
-echo started ssh connection to the expected host via ssh 
-sshpass -p EC2FakeUSer ssh -o StrictHostKeyChecking=no ec2-fake-user@10.1.0.5 whoami
+echo -e "\n" started ssh connection to the expected host via ssh
+USERNAME=$(sshpass -p EC2FakeUSer ssh -o StrictHostKeyChecking=no ec2-fake-user@10.1.0.5 whoami)
 
-exit
+if [ "$USERNAME" != "ec2-fake-user" ] 
+then
+    exit 1
+else
+    echo "[OK]"
+fi
