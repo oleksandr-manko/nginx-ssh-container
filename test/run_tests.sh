284c158205648ca6b135280721db6058955f4f7b
Alex Manko <oleksandr_manko@epam.com>
2019-08-22 04:23:21 +0300

    configuration sticks to the spec v3

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,14 +3,14 @@
 echo user ==> $(whoami)
 echo path ==> $(pwd)
 
-echo started scaning 10.5.0.1/24 network
-nmap 10.5.0.1/24
+echo started scaning 10.1.0.1/24 network
+nmap 10.1.0.1/24
 
-echo started scaning expected host 10.5.0.5
-nmap 10.5.0.5
+echo started scaning expected host 10.1.0.5
+nmap 10.1.0.5
 
 echo started ssh connection to the expected host via ssh 
-sshpass -p EC2FakeUSer ssh ec2-fake-user@10.5.0.5
+sshpass -p EC2FakeUSer ssh ec2-fake-user@10.1.0.5
 
 echo user ==> $(whoami)
 echo path ==> $(pwd)
